aa677d69bb8605cfd70472fae5529661d63a7725
sortega87 <sebastian.ortega@mpimet.mpg.de>
2025-04-09 10:32:46 +0200

    Unfreezing the filter window

--- pywk99/filter/window.py
+++ pywk99/filter/window.py
@@ -9,10 +9,11 @@ from pywk99.waves import LinearWave
 
 DISPERSION_CURVE_POINTS = 100
 
+
 class FilterPoint(Point):
     """See shapely.geometry.Point."""
 
-@dataclass(frozen=True)
+
 class FilterWindow:
     name: str
     polygon: Union[Polygon, MultiPolygon]
