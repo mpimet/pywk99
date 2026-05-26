52aff75b29b2dc186d4d994db49308a5f47f53fe
sortega87 <sebastian.ortega@mpimet.mpg.de>
2026-05-26 13:28:21 +0200

    fixing spectra plotting functions

--- pywk99/spectrum/plot.py
+++ pywk99/spectrum/plot.py
@@ -21,17 +21,17 @@ def plot_spectrum(spectrum: xr.DataArray,
                   cmap: str = "magma",
                   **kwargs) -> None:
     """Plot a longitude-time Power Spectrum."""
-    plot_spectrum = spectrum.copy()
+    new_spectrum = spectrum.copy()
     if ax is None:
         ax = plt.gca()
     if flagged_windows:
-        plot_spectrum = modify_spectrum(
-            plot_spectrum, flagged_windows, 'substract'
+        new_spectrum = modify_spectrum(
+            new_spectrum, flagged_windows, 'substract'
             )
     if use_log:
         with np.errstate(divide='ignore'):
-            plot_spectrum = np.log10(plot_spectrum)
-    plot_spectrum.sel(
+            new_spectrum = np.log10(new_spectrum)
+    new_spectrum.sel(
         wavenumber=slice(k_min, k_max)
     ).plot.contourf(levels=50,
                     cmap=cmap,
@@ -55,7 +55,7 @@ def plot_spectrum_peaks(spectrum: xr.DataArray,
         ax = plt.gca()
     if flagged_windows:
         spectrum_peaks = modify_spectrum(
-            plot_spectrum, flagged_windows, 'substract'
+            spectrum_peaks, flagged_windows, 'substract'
             )
     spectrum_peaks.sel(wavenumber=slice(k_min, k_max)
                        ).plot.contourf(levels=np.arange(1.1, 2.0, 0.1),
@@ -75,15 +75,15 @@ def plot_coherence(cross_spectrum: xr.DataArray,
                    cmap: str = "YlOrRd",
                    **kwargs) -> None:
     """Plot a longitude-time Power Spectrum."""
-    plot_spectrum = cross_spectrum.copy()
+    new_spectrum = cross_spectrum.copy()
     if ax is None:
         ax = plt.gca()
     if flagged_windows:
-        plot_spectrum = modify_spectrum(
-            plot_spectrum, flagged_windows, 'substract'
+        new_spectrum = modify_spectrum(
+            new_spectrum, flagged_windows, 'substract'
             )
     # compute coherence and phase arrows
-    coh2 = plot_spectrum.coherence_squared
+    coh2 = new_spectrum.coherence_squared
     coh2 = coh2.where(coh2 > coherence_threshold, drop=True)
     # plot
     coh2.sel(
@@ -102,20 +102,20 @@ def plot_phase_arrows(cross_spectrum: xr.DataArray,
                       flagged_windows: Optional[FilterWindow] = None,
                       **kwargs) -> None:
     """Plot a longitude-time Power Spectrum."""
-    plot_spectrum = cross_spectrum.copy()
+    new_spectrum = cross_spectrum.copy()
     if ax is None:
         ax = plt.gca()
     if flagged_windows:
-        plot_spectrum = modify_spectrum(
-            plot_spectrum, flagged_windows, 'substract'
+        new_spectrum = modify_spectrum(
+            new_spectrum, flagged_windows, 'substract'
             )
     # compute coherence and phase arrows
-    coh2 = plot_spectrum.coherence_squared
+    coh2 = new_spectrum.coherence_squared
     coh2 = coh2.where(coh2 > coherence_threshold, drop=True)
-    u = xr.apply_ufunc(np.real, 1j * plot_spectrum.cross )
-    u = u / np.abs(plot_spectrum.cross)
-    v = xr.apply_ufunc(np.imag, 1j * plot_spectrum.cross )
-    v = v / np.abs(plot_spectrum.cross)
+    u = xr.apply_ufunc(np.real, 1j * new_spectrum.cross )
+    u = u / np.abs(new_spectrum.cross)
+    v = xr.apply_ufunc(np.imag, 1j * new_spectrum.cross )
+    v = v / np.abs(new_spectrum.cross)
     u = u.where(coh2 > coherence_threshold, drop=True)
     v = v.where(coh2 > coherence_threshold, drop=True)
     # plot
